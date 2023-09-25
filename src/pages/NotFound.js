e56ea9b2c06a8cfcf9074a4a2583351a606c286e
AntunesD <antunesd90@gmail.com>
2023-09-25 16:57:12 +0200

    Étape 6 : Créez la page d’erreur

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 
 function NotFound() {
   return (
-    <div>
+    <div className='notFound'>
       <h1>404</h1>
       <p>Oups! La page que vous demandez n'existe pas.</p>
       <Link to="/">Retourner sur la page d'acceuil</Link>
